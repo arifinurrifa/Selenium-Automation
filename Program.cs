5c248f97c18341c630f721d3cc2f90b79a0490cc
arifinurrifa <rifa00rahman@gmail.com>
2022-04-26 02:44:10 +0600

    First commit for this project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Automation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            IWebElement Search = driver.FindElement(By.Name("q"));
+            driver.Manage().Window.Maximize();
+
+            Search.SendKeys("HIIIIIIIIIIIIIIII");
+
+            driver.Quit();
+
+        }
+    }
+
+}
+        
+    
+
